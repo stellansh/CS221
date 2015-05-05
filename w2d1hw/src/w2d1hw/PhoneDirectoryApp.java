a6d0fcb79367a10e6a20d08de159d38b74c8f68e
stellansh <stellanshuti2@gmail.com>
2015-05-05 09:11:47 -0500

    hw
    hw

--- w2d1hw/src/w2d1hw/PhoneDirectoryApp.java
+++ w2d1hw/src/w2d1hw/PhoneDirectoryApp.java
@@ -13,16 +13,24 @@ public class PhoneDirectoryApp {
     
     
     public static void main(String[] args){
-       DirectoryOpr pD = new DirectoryOpr();
-        pD.insertFirst("a", 23534344);
-        pD.insertFirst("b", 78734342);
-        pD.insertLast("c", 128834342);
-        pD.insertLast("d", 323484342);
-        pD.displayList();
-        System.out.println("Size: " + pD.size());
-        System.out.println(pD.find("c"));
-        System.out.println("Delete first: " + pD.deleteFirst());
+       DirectoryOpr dirr = new DirectoryOpr();
+        dirr.insertFirst("a", 1111111);
+        dirr.insertFirst("b", 72222);
+        dirr.insertLast("c", 333332);
+        dirr.insertLast("d", 44444442);
+        dirr.displayList();
+        System.out.println("Size: " + dirr.size());
+        System.out.println(dirr.find("c"));
+        System.out.println("Delete first: " + dirr.deleteFirst());
         
     }
 }
-
+/*Output:
+Name: a, Phone: 1111111
+Name: b, Phone: 722222
+Name: c, Phone: 333332
+Name: d, Phone: 44444442
+Size: 4
+Directory{name=c, pno=333332}
+Delete first: Directory{name=a, pno=111111}
+*/
